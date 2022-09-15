731e17010090a49aa361b4ecf55e0afa5788eee0
sasnigdho <salmanahmedims@gmail.com>
2022-09-15 23:32:20 +0600

    Debugger test done.

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 10;
+if (age < 50)
+    age = 12;
+console.log("Exiting with age: " + age + ".");
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 let age: number = 10;
+if (age < 50) age = 12;
+console.log("Exiting with age: " + age + ".");
